2d135777b7ab663634c3ddceed68d97042a26753
TJS Capita <trevor.smith@capita.com>
2022-11-24 14:57:18 +0000

    Initial commit

---
+++ src/DemoCalculator.java
@@ -0,0 +1,10 @@
+public class Program {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.num1 = 2.1;
+        calc.num2 = 3.3;
+//        calc.adds(calc.num1, calc.num2);
+        System.out.println(calc.total);
+    }
+}
+
